3de201fe9e74ccc2f2676bf4c5b33918161b6ae0
Rafael Thayto <rafa.thayto@gmail.com>
2023-04-15 18:08:27 -0300

    chore: adds initial project structure using expressots

---
+++ src/app.usecase.ts
@@ -0,0 +1,9 @@
+import { provide } from "inversify-binding-decorators";
+@provide(AppUseCase)
+class AppUseCase {
+    execute() {
+        return "Hello Expresso TS!";
+    }
+}
+
+export { AppUseCase };

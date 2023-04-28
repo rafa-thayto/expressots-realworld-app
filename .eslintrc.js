42019992c336adb8596a2648067ee933c90bec2e
Rafael Thayto <rafa.thayto@gmail.com>
2023-04-27 22:13:37 -0300

    chore: update many config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,9 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'no-trailing-spaces': ['error', { skipBlankLines: true }],
+    // 'no-multi-spaces': ['error', { ignoreEOLComments: true }],
+    // 'no-multi-spaces': 'off',
     // 'prettier/prettier': [
     //   'error',
     //   {

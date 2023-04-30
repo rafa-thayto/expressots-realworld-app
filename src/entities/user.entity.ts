0a1d39bf67f95e4ec9d60458cbdb46ddd28a284b
Rafael Thayto <rafa.thayto@gmail.com>
2023-04-29 23:06:01 -0300

    feat: update user entity

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -2,11 +2,10 @@ import { provide } from 'inversify-binding-decorators'
 
 @provide(User)
 export class User {
-  public id: number
   public token?: string
   public email: string
   public username: string
   public password: string
-  public bio: string
-  public image: string
+  public bio?: string
+  public image?: string
 }

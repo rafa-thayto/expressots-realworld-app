62200f8f6351cebc85b622a671d1328c22ef47a5
Rafael Thayto <rafa.thayto@gmail.com>
2023-05-11 22:04:04 -0300

    feat: adds JWT_SECRET on env

--- src/env.ts
+++ src/env.ts
@@ -1,4 +1,4 @@
-import pkg from "../package.json";
+import pkg from '../package.json'
 
 const ENV = {
     Application: {
@@ -6,11 +6,12 @@ const ENV = {
         APP_VERSION: pkg.version,
         ENVIRONMENT: process.env.ENVIRONMENT as string,
         PORT: Number(process.env.PORT),
+        JWT_SECRET: process.env.JWT_SECRET as string,
     },
     Log: {
         FILE: process.env.FILE as string,
         FOLDER: process.env.FOLDER as string,
     },
-};
+}
 
-export default ENV;
+export default ENV

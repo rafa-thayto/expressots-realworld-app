cd688683ccb2a8d4d79cda079517d7e62da20413
Rafael Thayto <rafa.thayto@gmail.com>
2023-05-11 22:25:43 -0300

    fix: create-user user case

--- src/useCases/user/create/create-user.usecase.ts
+++ src/useCases/user/create/create-user.usecase.ts
@@ -3,7 +3,7 @@ import { provide } from 'inversify-binding-decorators'
 import { CreateUserRequestDTO, CreateUserResponseDTO } from './create-user.dto'
 import { UserRepository } from '@repositories/user/user.repository'
 import { genSaltSync, hashSync } from 'bcrypt'
-import { Prisma } from '@prisma/client'
+import { Prisma, User } from '@prisma/client'
 
 @provide(CreateUserUseCase)
 class CreateUserUseCase {
@@ -15,10 +15,12 @@ class CreateUserUseCase {
             const hashedPassword = hashSync(data.user.password, salt)
 
             await this.userRepository.create({
+                image: null,
+                bio: null,
                 email: data.user.email,
                 password: hashedPassword,
                 username: data.user.username,
-            })
+            } as User)
 
             const response: CreateUserResponseDTO = {
                 status: StatusCode.OK,

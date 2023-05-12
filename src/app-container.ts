368259f91693091d851a35c48c1afbe98d898cf9
Rafael Thayto <rafa.thayto@gmail.com>
2023-05-11 22:21:45 -0300

    feat: adds UsersModule on appContainer

--- src/app-container.ts
+++ src/app-container.ts
@@ -1,15 +1,17 @@
-import { AppContainer } from "@expressots/core";
-import { AppModule } from "@useCases/app/app.module";
-import { PingModule } from "@useCases/ping/ping.module";
-import { UserModule } from "@useCases/user/user.module";
+import { AppContainer } from '@expressots/core'
+import { AppModule } from '@useCases/app/app.module'
+import { PingModule } from '@useCases/ping/ping.module'
+import { UserModule } from '@useCases/user/user.module'
+import { UsersModule } from '@useCases/users/users.module'
 
-const appContainer = new AppContainer();
+const appContainer = new AppContainer()
 
 const container = appContainer.create([
     // Add your modules here
     AppModule,
     PingModule,
     UserModule,
-]);
+    UsersModule,
+])
 
-export { container };
+export { container }
